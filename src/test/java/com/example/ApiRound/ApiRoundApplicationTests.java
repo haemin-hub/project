c18b490a5d69b4c39ab5dac5873ec663131fd933
k-hyeonah <hannah6394@gmail.com>
2025-08-22 17:36:42 +0900

    첫 커밋

---
+++ src/test/java/com/example/ApiRound/ApiRoundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ApiRound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
