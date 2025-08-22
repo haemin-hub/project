cb79227307dc0387b3d288f13b1680f34fe28b83
k-hyeonah <hannah6394@gmail.com>
2025-08-22 17:36:42 +0900

    2커밋

--- src/main/java/com/example/ApiRound/ApiRoundApplication.java
+++ src/main/java/com/example/ApiRound/ApiRoundApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApiRoundApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(ApiRoundApplication.class, args);
 	}
 
