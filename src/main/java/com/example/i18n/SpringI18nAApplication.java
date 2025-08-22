3652fb307f864cad90fd33826c1ffc382c6017cc
정민서 <hanyu0235@gmail.com>
2025-08-22 17:44:25 +0900

    Initial commit on mingzz

---
+++ src/main/java/com/example/i18n/SpringI18nAApplication.java
@@ -0,0 +1,12 @@
+
+package com.example.i18n;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringI18nAApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringI18nAApplication.class, args);
+    }
+}
