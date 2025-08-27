de1e7396b819f80b4688e0bbff8f21723199581b
haemin-hub <khmin55555@gmail.com>
2025-08-27 15:54:12 +0900

    업체 클릭 수 집계해서 main.jsp에 top3 뿌리기(실제 업체 클릭은 아직 구현 안함, DB에서 insert문 실행해서 확인해야함) application.properties에 본인 db 이름으로 되어있는지 꼭!!!!!!!체크

---
+++ src/main/java/com/example/ApiRound/Service/ClickLogServiceImpl.java
@@ -0,0 +1,36 @@
+package com.example.ApiRound.Service;
+
+import com.example.ApiRound.dto.CompanyClickCountDto;
+import com.example.ApiRound.mapper.ClickLogMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClickLogServiceImpl implements ClickLogService {
+
+
+    private final ClickLogMapper clickLogMapper;
+
+    public ClickLogServiceImpl(ClickLogMapper clickLogMapper) {
+        this.clickLogMapper = clickLogMapper;
+    }
+
+    @Override
+    public void logClick(Long companyId) {
+            System.out.println(">>> 클릭된 업체 ID: " + companyId);
+            clickLogMapper.insertClickLog(companyId);
+        }
+
+
+    @Override
+    public List<CompanyClickCountDto> getClickCountsLast7Days() {
+        return clickLogMapper.getClickCountsLast7Days();
+    }
+
+    @Override
+    public List<CompanyClickCountDto> getTop3CompaniesLast7Days() {
+        return clickLogMapper.getTop3CompaniesLast7Days();
+    }
+
+}
